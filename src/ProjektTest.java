01fa2d856dfe041ccbd106fbf92d7c960795c64b
rogerpietrzak <rogerpietrzak92@gmail.com>
2019-12-07 11:28:51 +0100

    First commit to project.

---
+++ src/ProjektTest.java
@@ -0,0 +1,12 @@
+/**
+ * ...comment class...
+ *
+ * @author
+ * @since 07.12.2019
+ */
+public class ProjektTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
